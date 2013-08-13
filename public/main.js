34b315feb5678e81cd651e61530c438147778e17
Andrew Kelley <superjoe30@gmail.com>
2013-08-12 22:24:14 -0400

    basic socket connecting and sending message

--- public/main.js
+++ public/main.js
@@ -1,1 +1,40 @@
-// TODO: anything
+(function() {
+  var host = window.document.location.host;
+  var pathname = window.document.location.pathname;
+  var match = host.match(/^(.+):(\d+)$/);
+  var port = match ? parseInt(match[2], 10) : 443;
+  var hostName = match ? match[1] : host;
+  var wsUrl = 'ws://' + hostName + ':' + port + pathname;
+  var ws = createWs();
+
+  var domStatus = document.getElementById("status");
+  var domButton = document.getElementById("button");
+  domButton.addEventListener("click", doIt, false);
+
+  function doIt() {
+    ws.send(JSON.stringify({name: 'gimmeSomeOgg'}));
+  }
+
+  function createWs() {
+    var newWs = new WebSocket(wsUrl);
+    newWs.onmessage = function(event) {
+      domStatus.innerHTML = "message: " + event.data;
+    };
+    newWs.onerror = function(event) {
+      domStatus.innerHTML = "error. reconnecting...";
+      setTimeout(function() {
+        ws = createWs();
+      }, 1000);
+    };
+    newWs.onclose = function(event) {
+      domStatus.innerHTML = "closed. reconnecting...";
+      setTimeout(function() {
+        ws = createWs();
+      }, 1000);
+    };
+    newWs.onopen = function() {
+      domStatus.innerHTML = "open";
+    };
+    return newWs;
+  }
+})();
--- server.js
+++ server.js
@@ -2,11 +2,59 @@ var express = require('express');
 var ws = require('ws');
 var path = require('path');
 var http = require('http');
-
 var app = express();
-app.use(express.static(path.join(__dirname, "public")));
 var server = http.createServer(app);
-server.listen(process.env.PORT || 10491, function() {
+
+// users may never send a web socket message longer than this
+var MAX_SOCKET_MSG_LEN = 500;
+
+var wss = new ws.Server({
+  server: server,
+  clientTracking: false,
+});
+
+wss.on('connection', onSocketConnection);
+
+app.use(express.static(path.join(__dirname, "public")));
+server.listen(process.env.PORT || 10491, onServerListening);
+
+function onServerListening() {
   console.info("Listening at http://0.0.0.0:" +
     server.address().port + "/");
-});
+}
+
+function onSocketConnection(socket) {
+  var msgHandlers = {
+    gimmeSomeOgg: gimmeSomeOgg,
+  };
+  socket.on('message', onMessage);
+
+  function gimmeSomeOgg(msg) {
+    socket.send(new Buffer("aoeu"));
+  }
+
+  function onMessage(data, flags) {
+    if (flags.binary) {
+      console.warn("WARNING: binary web socket message received");
+      return;
+    }
+    if (data.length > MAX_SOCKET_MSG_LEN) {
+      console.warn("WARNING: received web socket message of length",
+        data.length, "max:", MAX_SOCKET_MSG_LEN);
+      return;
+    }
+    var msg;
+    try {
+      msg = JSON.parse(data);
+    } catch (err) {
+      console.warn("WARNING: received invalid JSON from web socket", err.message);
+      return;
+    }
+    var handler = msgHandlers[msg.name];
+    if (! handler) {
+      console.warn("WARNING: no message handler for", msg.name);
+      return;
+    }
+    handler(msg.value);
+  }
+}
